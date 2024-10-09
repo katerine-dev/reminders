ee0d23fa4fde35d83266a39db7e05d4ee1e6f5de
katewitkoski <ka.lwitkoski@gmail.com>
2024-10-09 18:22:22 -0300

    inicia o módulo das tarefas completas

--- spa/src/App.jsx
+++ spa/src/App.jsx
@@ -1,15 +1,62 @@
+import { useState } from "react";
 import AddReminder from "./components/AddReminder";
 import Reminder from "./components/Reminder";
 
 function App() {
+  const [reminder, setReminder] = useState([
+    {
+      id: 1,
+      title: "Buy groceries",
+      description: "Milk, bread, eggs, and fruits",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      title: "Finish project report",
+      description: "Complete the final draft and send it to the team",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      title: "Schedule doctor appointment",
+      description: "Call to book an appointment for next week",
+      isCompleted: false,
+    },
+    {
+      id: 4,
+      title: "Exercise",
+      description: "Go for a 30-minute run or workout",
+      isCompleted: false,
+    },
+    {
+      id: 5,
+      title: "Read a book",
+      description: "Finish reading 'The Great Gatsby'",
+      isCompleted: false,
+    },
+  ]); //lista
+
+  function onReminderClick(reminderId) {
+    const newReminder = reminder.map((reminder) => {
+      // PRECISO ATUALIZAR ESSA TAREFA
+      if (reminder.id == reminderId) {
+        return { ...reminder, isCompleted: !reminder.isCompleted };
+      }
+
+      // Não precisa atualizar essa tarefa
+      return reminder;
+    });
+  }
+
   return (
     <div className="w-screen h-screen bg-background bg-cover bg-center flex justify-center p-6">
+      {/*O theme está no módulo de configuração do tailwind, o responsável em fazer o thema aparever é bg-background*/}
       <div className="w-[500px]">
         <h1 className="text-6xl text-white font-bold text-center drop-shadow-lg">
           Reminders
         </h1>
         <AddReminder />
-        <Reminder />
+        <Reminder reminder={reminder} />
       </div>
     </div>
   );
--- spa/src/components/Reminder.jsx
+++ spa/src/components/Reminder.jsx
@@ -1,5 +1,23 @@
-function Reminder() {
-  return <h1>hjsdjhaskd</h1>;
+import { ChevronRight } from "lucide-react";
+
+function Reminder(props) {
+  return (
+    <ul className="space-y-4 p-5 bg-slate-200 rounded-md shadow">
+      {props.reminder.map((reminder) => (
+        <li key={reminder.id} className="flex gap-2">
+          <button
+            onClick={() => props.onReminderClick(reminder.id)}
+            className="bg-slate-400 text-left w-full text-white p-2 rounded-md"
+          >
+            {reminder.title}
+          </button>
+          <button className="bg-slate-400 text-white p-2 rounded-md">
+            <ChevronRight />
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
 }
 
 export default Reminder;
