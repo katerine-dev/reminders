97d4abab6e0bec9ea7a2d7eabe0f5aa8c98ea6f5
katewitkoski <ka.lwitkoski@gmail.com>
2024-10-15 09:55:53 -0300

    add footer

--- spa/src/components/Footer.jsx
+++ spa/src/components/Footer.jsx
@@ -2,7 +2,7 @@ function Footer() {
   return (
     <footer className="flex flex-col items-start text-xs text-white">
       <div className="w-fullp-4 text-center">
-        Copyright ©2024 |
+        2024 |
         <a href="https://github.com/katerine-dev"> Made with love by Kate</a>
       </div>
     </footer>
