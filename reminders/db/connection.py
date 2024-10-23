fd0c25f67d4b562c1e862f84934fac22d8cacf62
katewitkoski <ka.lwitkoski@gmail.com>
2024-10-23 17:17:44 -0300

    add env var support

--- reminders/db/connection.py
+++ reminders/db/connection.py
@@ -1,8 +1,16 @@
 # Return a connection provided by psycopg.
 import psycopg
+import os
+from dotenv import load_dotenv
 
-#TODO: mover confgiuracao do banco para variaveis de ambiente
+load_dotenv()
+
 def get_connection():
     return psycopg.connect(
-        "dbname=reminders host=localhost user=reminders password=reminders port=5432", autocommit=True
+        dbname=os.getenv("DB_NAME"),
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        port=os.getenv("DB_PORT"),
+        autocommit=True
     )
