a76a24e809e26fb2b2d9fba65cc142a3d925c390
katewitkoski <ka.lwitkoski@gmail.com>
2024-10-30 07:46:15 -0300

    change prefix for static

--- reminders/main.py
+++ reminders/main.py
@@ -13,11 +13,12 @@ load_dotenv()
 # Create an instance of the FastAPI app
 app = FastAPI()
 
-# Inclui o roteador de lembretes com prefixo "/reminders"
-app.include_router(reminder_route, prefix="/reminders")
+# Inclui o roteador de lembretes com prefixo "/static"
+app.include_router(reminder_route, prefix="/static")
 
 static_dir = os.path.join(os.path.dirname(__file__), "../spa/dist")
-app.mount("/reminders", StaticFiles(directory=static_dir), name="reminders")
+
+app.mount("/static", StaticFiles(directory=static_dir), name="static")
 
 # Custom exception handler for validation errors:
 # Forces FastAPI to return a 400 Bad Request status code instead of the default 422 Unprocessable Entity
@@ -28,8 +29,9 @@ async def validation_exception_handler(_request, exception):
 @app.get("/", include_in_schema=False)
 @app.head("/", include_in_schema=False)
 async def root():
-    return RedirectResponse(url="/reminders/index.html")
+    return RedirectResponse(url="/static/index.html")
 
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
+
